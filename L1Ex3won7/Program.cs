8c4cc4c0f6f098405f779a5fa0b430cab73ebee5
Cristiano Py <Cristiano Py@DESKTOP-U2EFTL3>
2024-02-22 10:44:29 +0200

    Add project files.

---
+++ L1Ex3won7/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+tastaura*/
+
+int num;
+Console.WriteLine("Introduce un numar: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int ln = num % 10;
+
+Console.WriteLine("Ultima cifra a unui numar intreg este: " +  ln);
+
+Console.ReadLine();
